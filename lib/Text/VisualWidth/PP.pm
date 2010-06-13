3c2d4ac1c50c678d5c04db760b9247208b620c62
tokuhirom <tokuhirom@gmail.com>
2010-06-14 00:09:39 +0900

    initial import

---
+++ lib/Text/VisualWidth/PP.pm
@@ -0,0 +1,39 @@
+package Text::VisualWidth::PP;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Text::VisualWidth::PP -
+
+=head1 SYNOPSIS
+
+  use Text::VisualWidth::PP;
+
+=head1 DESCRIPTION
+
+Text::VisualWidth::PP is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
