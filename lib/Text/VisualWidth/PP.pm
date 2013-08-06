50c77dcf46b12964bac4848e409245c0b52a2401
tokuhirom <tokuhirom@gmail.com>
2013-08-06 13:03:19 +0900

    Treat ambigious width.
    Added $Text::VisualWidth::PP::EastAsian flag for better ambigious width
    control.

--- lib/Text/VisualWidth/PP.pm
+++ lib/Text/VisualWidth/PP.pm
@@ -5,12 +5,38 @@ use 5.008001;
 our $VERSION = '0.01';
 use Unicode::EastAsianWidth;
 
+our $EastAsian = $Unicode::EastAsianWidth::EastAsian;
+
+sub InVWPP1Fullwidth() {
+    InEastAsianFullwidth() . InEastAsianWide() . InEastAsianAmbiguous()
+}
+sub InVWPP0Fullwidth() {
+    InEastAsianFullwidth() . InEastAsianWide()
+}
+sub InVWPP1Halfwidth() {
+    InEastAsianHalfwidth().
+    InEastAsianNarrow().
+    InEastAsianNeutral()
+}
+sub InVWPP0Halfwidth() {
+    InEastAsianHalfwidth().
+    InEastAsianNarrow().
+    InEastAsianNeutral().
+    InEastAsianAmbiguous()
+}
+
 sub width {
     my $str = shift;
 
     my $ret = 0;
-    while ($str =~ /(\p{InFullwidth}+)|(\p{InHalfwidth}+)/g) {
-        $ret += $1 ? length($1) * 2 : length($2)
+    if ($EastAsian) {
+        while ($str =~ /(\p{InVWPP1Fullwidth}+)|(\p{InVWPP1Halfwidth}+)/g) {
+            $ret += $1 ? length($1) * 2 : length($2)
+        }
+    } else {
+        while ($str =~ /(\p{InVWPP0Fullwidth}+)|(\p{InVWPP0Halfwidth}+)/g) {
+            $ret += $1 ? length($1) * 2 : length($2)
+        }
     }
     $ret;
 }
@@ -62,6 +88,14 @@ Text::VisualWidth::PP - trimming text by the number of the column s of terminals
 This module provides functions to treat half-width and full-width characters and display correct size of text in one line on terminals and mobile phones. You can know the visual width of any text and truncate text by the visual width. Now this module support flagged UTF-8 and tested only with Japanese.  
 
 This module is pure perl version of L<Text::VisualWidth>. This is bit slow, but it's not require compiler.
+
+=head1 Ambiguous Characters
+
+This module treats ambiguous characters are half width by default.
+
+And you can specify the behavior by the C<$Text::VisualWidth::PP::EastAsian> flag expressly.
+
+Note: If C<$Unicode::EastAsianWidth::EastAsian> is true on compilation time, this module set C<$Text::VisualWidth::PP::EastAsian> as true for backward compatibility.
 
 =head1 AUTHOR
 
